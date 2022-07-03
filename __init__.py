4861173bb263ea6895739646164e2ffa9b9dafaa
serggom <sergio.s.gomezconde@gmail.com>
2022-07-03 16:35:50 +0200

    Avances

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
+import json
+import os
+
 from mycroft import MycroftSkill, intent_file_handler
 
+# Fichero JSON donde almacenar la informacion
+ficheroJSON = "/home/serggom/scraping/datos.json"
 
 class LeerMensajesCampus(MycroftSkill):
     def __init__(self):
@@ -7,8 +12,29 @@ class LeerMensajesCampus(MycroftSkill):
 
     @intent_file_handler('campus.mensajes.leer.intent')
     def handle_campus_mensajes_leer(self, message):
-        self.speak_dialog('campus.mensajes.leer')
 
+        # Lectura de la informacion del fichero JSON
+        if os.path.exists(ficheroJSON):
+            # Lectura de la informacion del fichero JSON
+            with open(ficheroJSON) as ficheroMensajes:
+                data = json.load(ficheroMensajes)
+
+                numero_mensajes = data['numero_mensajes'][0]['total_privados']
+
+                if int(numero_mensajes) > 0:
+                    self.speak("Tienes " + numero_mensajes + " mensajes")
+
+                    for mensaje in data['mensajes']:
+                        self.speak("Mensaje de " + mensaje['autor'] + " el " + mensaje['fecha'] + ":")
+                        self.speak(mensaje['contenido'])
+
+                else:
+                    self.speak("No tienes mensajes que leer")
+
+            ficheroMensajes.close()
+
+        else:
+            self.speak("Lo siento, no dispongo de esa información")
 
 def create_skill():
     return LeerMensajesCampus()
